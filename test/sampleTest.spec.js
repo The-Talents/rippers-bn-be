ab8e6a2abee5c75bc0e5abeec083c0a777a7b1d8
Naome Joyeuse <naomejoyce5@gmail.com>
2025-01-07 13:10:59 +0200

    Update sampleTest.spec.js

--- test/sampleTest.spec.js
+++ test/sampleTest.spec.js
@@ -1,8 +1,20 @@
 
+// import { expect } from 'chai';
+
+// describe('Sample Test', () => {
+//   it('should pass a basic assertion', () => {
+//     expect(1 + 1).to.equal(2);
+//   });
+// });
+
 import { expect } from 'chai';
 
-describe('Sample Test', () => {
-  it('should pass a basic assertion', () => {
-    expect(1 + 1).to.equal(2);
+describe('Simple Math Test', () => {
+  it('should return 4 when 2 is added to 2', () => {
+    expect(2 + 2).to.equal(4);
+  });
+
+  it('should return 9 when 3 is multiplied by 3', () => {
+    expect(3 * 3).to.equal(9);
   });
 });
