d8a0210ece1202e544cd2e4b9aaf2fcb8bc166d8
NaomeJoyeuse <naomejoyce5@gmail.com>
2025-01-13 09:13:14 +0200

    test db connection3

--- test/sampleTest.test.js
+++ test/sampleTest.test.js
@@ -1,43 +1,3 @@
-
-// const chai = require('chai');
-// const chaiHttp = require('chai-http');
-// const { app, db } = require('../src/sample_dbTest.js'); 
-
-// const expect = chai.expect;
-// chai.use(chaiHttp);
-
-// describe('Database Connection Test', () => {
-//   before(async () => {
-//     if (!db?.sequelize) {
-//       throw new Error('Database connection not initialized');
-//     }
-//     await db.sequelize.sync();
-//   });
-
-//   after(async () => {
-//     if (db?.sequelize) {
-//       await db.sequelize.close();
-//     }
-//   });
-
-//   it('should connect to the database successfully', (done) => {
-//     chai.request(app)
-//       .get('/test-db')
-//       .end((err, res) => {
-//         expect(res).to.have.status(200);
-//         expect(res.body).to.have.property('message').eql('Database connection successful!');
-//         done();
-//       });
-//   });
-
-//   // it('should fail to connect to the database', async () => {
-//   //   await db.sequelize.close(); 
-//   //   const res = await chai.request(app).get('/test-db');
-//   //   expect(res).to.have.status(500);
-//   //   expect(res.body).to.have.property('message').eql('Database connection failed!');
-//   // });
-// });
-
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const { app, db } = require('../src/sample_dbTest.js'); 
@@ -50,30 +10,22 @@ describe('Database Connection Test', () => {
     if (!db?.sequelize) {
       throw new Error('Database connection not initialized');
     }
-    await db.sequelize.sync();
+    await db.sequelize.sync();  // Synchronize database models
   });
 
   after(async () => {
     if (db?.sequelize) {
-      await db.sequelize.close();
+      await db.sequelize.close();  // Close the database connection
     }
   });
 
-  it('should connect to the database successfully', (done) => {
-    chai.request(app)
-      .get('/test-db')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.body).to.have.property('message').eql('Database connection successful!');
-        done();
-      });
+  it('should connect to the database and close connection successfully', async () => {
+    try {
+      await db.sequelize.authenticate();  // Test database connection
+      expect(true).to.be.true;  // If no error, the connection is successful
+    } catch (error) {
+      console.error('Database connection failed:', error);
+      throw new Error('Database connection failed!');
+    }
   });
-
-  // You may uncomment this for testing a failed database connection
-  // it('should fail to connect to the database', async () => {
-  //   await db.sequelize.close(); 
-  //   const res = await chai.request(app).get('/test-db');
-  //   expect(res).to.have.status(500);
-  //   expect(res.body).to.have.property('message').eql('Database connection failed!');
-  // });
 });
