bffeb7cf62dee835636e3dc8cd9b2e32334a9a4d
NaomeJoyeuse <naomejoyce5@gmail.com>
2025-01-14 13:07:41 +0200

    updates on test

--- src/sample_dbTest.js
+++ src/sample_dbTest.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const db = require('../models/index.js'); // Adjust to your Sequelize models path
+const db = require('../models/index.js'); 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,17 +16,17 @@ app.get('/test-db', async (req, res) => {
 });
 
 
-// Route to create a new user
+
 app.post('/create-user', async (req, res) => {
   try {
     const { firstName, lastName, email, preferredLanguage, preferredCurrency, whereYouLive, role, department, lineManager, gender, telephoneNumber } = req.body;
 
-    // Validate required fields
+
     if (!firstName || !lastName || !email  || !gender || !telephoneNumber) {
       return res.status(500).json({ message: 'Missing required fields!' });
     }
 
-    // Create new user
+  
     const user = await db.User.create({
       firstName,
       lastName,
--- test/sampleTest.test.js
+++ test/sampleTest.test.js
@@ -1,37 +1,7 @@
-// const chai = require('chai');
-// const chaiHttp = require('chai-http');
-// const { app, db } = require('../src/sample_dbTest.js'); 
 
-// const expect = chai.expect;
-// chai.use(chaiHttp);
-
-// describe('Database Connection Test', () => {
-//   before(async () => {
-//     if (!db?.sequelize) {
-//       throw new Error('Database connection not initialized');
-//     }
-//     await db.sequelize.sync();  // Synchronize database models
-//   });
-
-//   after(async () => {
-//     if (db?.sequelize) {
-//       await db.sequelize.close();  // Close the database connection
-//     }
-//   });
-
-//   it('should connect to the database and close connection successfully', async () => {
-//     try {
-//       await db.sequelize.authenticate();  // Test database connection
-//       expect(true).to.be.true;  // If no error, the connection is successful
-//     } catch (error) {
-//       console.error('Database connection failed:', error);
-//       throw new Error('Database connection failed!');
-//     }
-//   });
-// });
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const { app, db } = require('../src/sample_dbTest.js'); // Adjust path as necessary
+const { app, db } = require('../src/sample_dbTest.js'); 
 
 const expect = chai.expect;
 chai.use(chaiHttp);
