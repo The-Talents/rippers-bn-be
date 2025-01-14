159db082fbcfb97aa6f7855d0ea3bfce620b3a09
Eric manishimwe <manishimweeric54@gmail.com>
2025-01-14 13:18:41 +0200

    update

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-import { config } from 'dotenv';
-import express from 'express';
+const dotenv = require('dotenv');
+const express = require('express');
 
-config(); 
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
