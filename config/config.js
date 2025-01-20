65c8cda66326859ab29f59c65a393d1f16df961f
NaomeJoyeuse <naomejoyce5@gmail.com>
2025-01-20 12:02:04 +0200

    test1

--- config/config.js
+++ config/config.js
@@ -10,12 +10,14 @@ module.exports = {
     dialect: 'postgres'
   },
   test: {
-    username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'password',
-    database: process.env.DB_NAME || 'database_test',
-    host: process.env.DB_HOST || '127.0.0.1',
+    username: process.env.POSTGRES_USER || 'postgres',
+    password: process.env.POSTGRES_PASSWORD || '1234',
+    database: process.env.POSTGRES_DB || 'barefoot_nomad',
+    host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    logging: false
+
   },
   production: {
     username: process.env.DB_USER || 'postgres',
