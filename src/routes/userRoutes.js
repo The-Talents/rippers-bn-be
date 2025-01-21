293a0b78fb503b47a89f65d0bd50f302928812aa
amini-ibeshaho <aminibeshaho@gmail.com>
2025-01-21 10:12:53 +0200

    testing on circle ci

--- src/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { userRegistrationController } = require('../controllers/userController');
-
-const router = express.Router();
-
-router.post('/register', userRegistrationController);
-
-module.exports = router;
